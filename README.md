4abbbe376491de598cafb2a9c03a65bdc22cd431
Jeff King <peff@peff.net>
2015-10-04 01:20:10 -0400

    initial commit
    At this point I have:
    
      - a board that isn't _too_ ugly
    
      - you can start a game to randomize the tiles; this
        will show all possibilities for each slot
    
      - you can choose or discard tiles by left- or
        right-clicking them
    
      - bogus choices are rejected with a message
    
      - correct choices update the other possibilities in the
        same row
    
      - slots with only one possible symbol auto-choose the
        symbol
    
      - likewise, symbols with only one possible slot will
        auto-choose that slot
    
    See TODO for a list of missing bits.

---
+++ README.md
@@ -0,0 +1,7 @@
+Einstein Puzzle
+===============
+
+This is a Javascript rewrite of the "Einstein" logic puzzle game. The
+version I have played is made by Flowix Games, but that is apparently a
+port of an old DOS game called "Sherlock". This version is inspired by
+the Flowix version, but the code was written from scratch.
---
+++ einstein.js
@@ -0,0 +1,177 @@
+var defaultSymbols = [
+	["1", "2", "3", "4", "5", "6"],
+	["A", "B", "C", "D", "E", "F"],
+	["I", "II", "III", "IV", "V", "VI"],
+	// unicode dice
+	["&#x2680;", "&#x2681;", "&#x2682;", "&#x2683;", "&#x2684;", "&#x2685;"],
+	// unicode shapes
+	["&#x25b3;", "&#x25bd;", "&#x25a1;", "&#x25c7;", "&#x2b20;", "&#x25cb;"],
+	["+", "&#x2012;", "&#x00f7;", "x", "=", "√"]
+];
+
+function Puzzle(board, messages, symbols) {
+	symbols = symbols || defaultSymbols;
+
+	this.messages = messages;
+	this.rows = [];
+	for (var i = 0; i < symbols.length; i++)
+		this.rows[i] = new Row(this, symbols[i], board.insertRow());
+
+	this.clear = function() {
+		for (var i = 0; i < this.rows.length; i++)
+			this.rows[i].clear();
+		this.say("Einstein Puzzle");
+	}
+
+	this.newGame = function() {
+		for (var i = 0; i < this.rows.length; i++)
+			this.rows[i].newGame();
+	}
+
+	this.say = function(msg) {
+		this.messages.innerHTML = msg;
+	}
+
+	this.clear();
+}
+
+function shuffle(array) {
+	for (var i = array.length - 1; i >= 0; i--) {
+		var r = Math.floor(Math.random() * (i+1));
+		var tmp = array[i];
+		array[i] = array[r];
+		array[r] = tmp;
+	}
+	return array;
+}
+
+function Row(puzzle, symbols, display) {
+	this.puzzle = puzzle;
+	this.slots = [];
+	for (var i = 0; i < symbols.length; i++)
+		this.slots[i] = new Slot(this, symbols, display.insertCell());
+
+	this.clear = function() {
+		for (var i = 0; i < this.slots.length; i++)
+			this.slots[i].displaySingle(i);
+	}
+
+	this.newGame = function() {
+		var values = [];
+		for (var i = 0; i < this.slots.length; i++)
+			values.push(i);
+		shuffle(values);
+		for (var i = 0; i < this.slots.length; i++)
+			this.slots[i].newGame(values[i]);
+	}
+
+	this.removePossible = function(value) {
+		for (var i = 0; i < this.slots.length; i++)
+			this.slots[i].discard(value);
+	}
+
+	this.checkSingleton = function(value) {
+		var possible = this.slots.filter(
+			function(slot) { return slot.isPossible(value) });
+		if (possible.length == 1)
+			possible[0].choose(value);
+	}
+}
+
+function Slot(row, symbols, display) {
+	this.row = row;
+	this.symbols = symbols;
+	this.elem = display;
+	this.elem.className = "slot";
+
+	this.say = function(msg) this.row.puzzle.say(msg);
+
+	this.newGame = function(value) {
+		this.value = value;
+		this.possible = [];
+		for (var i = 0; i < this.symbols.length; i++)
+			this.possible.push(true);
+		this.displayPossible();
+	}
+
+	this.displaySingle = function(i) {
+		var tile = document.createElement("div");
+		tile.className = "single";
+		tile.innerHTML = this.symbols[i];
+		this.elem.innerHTML = '';
+		this.elem.appendChild(tile);
+		this.single = true;
+	}
+
+	this.displayPossible = function() {
+		// as close to square as we can
+		var table = document.createElement("table");
+		var rows = Math.floor(Math.sqrt(this.symbols.length));
+		var cols = Math.floor(this.symbols.length / rows);
+		for (var i = 0; i < rows; i++) {
+			var row = table.insertRow();
+			var lo = i * cols;
+			var hi = (i+1) * cols;
+			if (hi > this.symbols.length)
+				hi = this.symbols.length;
+			for (var j = lo; j < hi; j++) {
+				var cell = this.displayPossible[j] = row.insertCell();
+				cell.innerHTML = this.symbols[j];
+				cell.className = "possibility";
+				cell.addEventListener('click',
+					function(s, j) { return function() {
+						s.choose(j);
+					}}(this, j));
+				cell.addEventListener('contextmenu',
+					function(s, j) { return function(ev) {
+						ev.preventDefault();
+						s.discard(j);
+					}}(this, j));
+			}
+		}
+
+		this.elem.innerHTML = "";
+		this.elem.appendChild(table);
+		this.single = false;
+	}
+
+	this.choose = function(value) {
+		if (this.value == value) {
+			this.displaySingle(value);
+			this.row.removePossible(value);
+		} else {
+			this.say("Nope, not " + this.symbols[value]);
+		}
+	}
+
+	this.discard = function(value) {
+		if (this.single)
+			return;
+		if (this.value == value) {
+			this.say("Oops, it was " + this.symbols[value]);
+		} else {
+			this.possible[value] = false;
+			this.displayPossible[value].innerHTML = "";
+			this.displayPossible[value].className = "dead-possibility";
+			this.checkSingleton();
+			this.row.checkSingleton(value);
+		}
+	}
+
+	this.isPossible = function(value) {
+		return !this.single && this.possible[value];
+	}
+
+	this.checkSingleton = function() {
+		var count = 0;
+		var last;
+		for (var i = 0; i < this.possible.length; i++) {
+			if (this.possible[i]) {
+				count++;
+				last = i;
+			}
+		}
+		if (count == 1)
+			this.choose(last);
+	}
+}
